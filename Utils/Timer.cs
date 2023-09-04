2d47a6e2a559078725b4fc58096bff4857801c2c
Nimiod <Nimiod02@gmail.com>
2023-09-04 22:27:57 +0700

    Feat(Timer Util) - утилитка для таймера

---
+++ Utils/Timer.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections;
+using DG.Tweening;
+using UnityEngine;
+
+namespace Submodules.Common.Utils
+{
+    public class Timer
+    {
+        public Action<Timer> OnStart;
+        public Action<Timer> OnUpdate;
+        public Action<Timer> OnPause;
+        public Action<Timer> OnEnd;
+    
+        public float AllSeconds { get; private set; }
+        public float CurrentSeconds { get; private set; }
+        public float LastedSeconds => AllSeconds - CurrentSeconds;
+
+        
+        private Tween _cashedTimer;
+
+        public Timer(float seconds)
+        {
+            AllSeconds = seconds;
+            CurrentSeconds = 0;
+        }
+
+        public void Start()
+        {
+            _cashedTimer = DOVirtual.Float(CurrentSeconds, AllSeconds, AllSeconds, value => OnUpdate?.Invoke(this))
+                .OnStart(() => OnStart?.Invoke(this))
+                .OnPause(() => OnPause?.Invoke(this))
+                .OnComplete(() => OnEnd?.Invoke(this));
+        }
+
+        public void Resume()
+        {
+            _cashedTimer?.Play();
+        }
+        
+        public void Pause()
+        {
+            _cashedTimer?.Pause();
+        }
+
+        private IEnumerator TimerRoutine()
+        {
+            while (LastedSeconds > 0)
+            {
+                CurrentSeconds += Time.deltaTime;
+
+                yield return null;
+            }
+        }
+    }
+}
